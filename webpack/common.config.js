cf6bf525b1ca75b1b0a8bc02a288f6754d9f1739
dlion4 <dantemilimo@gmail.com>
2026-01-27 06:02:57 +0300

    updated the issue with the webpack fonts compilation

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -53,14 +53,14 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name]-[fullhash][ext][query]",
+          filename: "images/[name]-[contenthash][ext][query]",
         },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
         type: "asset/resource",
         generator: {
-          filename: "fonts/[name]-[fullhash][ext][query]",
+          filename: "fonts/[name]-[contenthash][ext][query]",
         },
       },
       {
