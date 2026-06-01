9265afce0f13bb1b94b1bafd5a7faff9ea2ebc1a
dlion4 <dantemilimo@gmail.com>
2026-06-01 08:36:44 +0300

    added tailwindcss and material ui support

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -6,94 +6,153 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { ProvidePlugin } = require("webpack");
 const Dotenv = require("dotenv-webpack");
 
+const isDev = process.env.NODE_ENV !== "production";
+
 module.exports = {
   target: "web",
+
   context: path.join(__dirname, "../"),
+
   entry: {
     project: path.resolve(__dirname, "../static/js/project"),
+    tailwind: path.resolve(__dirname, "../static/js/tailwind"),
     vendors: path.resolve(__dirname, "../static/js/vendors"),
   },
+
   output: {
-    path: path.resolve(__dirname, "../assets/webpack_bundles/"),
+    path: path.resolve(__dirname, "../assets/webpack_bundles"),
     publicPath: "/static/webpack_bundles/",
     filename: "js/[name]-[fullhash].js",
-    chunkFilename: "js/[name]-[hash].js",
+    chunkFilename: "js/[name]-[contenthash].js",
     clean: true,
   },
+
   plugins: [
     new BundleTracker({
-      path: path.resolve(path.join(__dirname, "../assets")),
+      path: path.resolve(__dirname, "../assets"),
       filename: "webpack-stats.json",
     }),
-    new WebpackBar({ name: "compiler" }),
-    new MiniCssExtractPlugin({ filename: "css/[name].[contenthash].css" }),
+
+    new WebpackBar({
+      name: "compiler",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+    }),
+
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, "../static/images"),
           to: "images",
+          noErrorOnMissing: true,
         },
       ],
     }),
-    new Dotenv({ path: path.resolve(__dirname, "../.env") }), 
+
+    new Dotenv({
+      path: path.resolve(__dirname, "../.env"),
+    }),
+
     new ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    axios: "axios",
-  }),
+      $: "jquery",
+      jQuery: "jquery",
+      axios: "axios",
+    }),
   ],
+
   module: {
     rules: [
+      // JavaScript
       {
         test: /\.[jt]sx?$/,
-        loader: "babel-loader",
         exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name]-[contenthash][ext][query]",
+        use: {
+          loader: "babel-loader",
         },
       },
+
+      // Images
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[contenthash][ext]",
+        },
+      },
+
+      // Fonts
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
         type: "asset/resource",
         generator: {
-          filename: "fonts/[name]-[contenthash][ext][query]",
+          filename: "fonts/[name]-[contenthash][ext]",
         },
       },
+
+      // CSS / SCSS / Tailwind
       {
-        test: /\.s?css$/i,
+        test: /\.(css|scss|sass)$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: isDev,
+            },
+          },
           {
             loader: "postcss-loader",
             options: {
-              postcssOptions: {
-                plugins: ["postcss-preset-env", "autoprefixer", "pixrem"],
-              },
+              sourceMap: isDev,
             },
           },
-          "sass-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDev,
+            },
+          },
         ],
       },
     ],
   },
+
   resolve: {
     modules: ["node_modules"],
-    extensions: [".js", ".jsx"],
+
+    extensions: [
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".json",
+    ],
+
     alias: {
       "@images": path.resolve(__dirname, "../static/images"),
+      "@js": path.resolve(__dirname, "../static/js"),
+      "@scss": path.resolve(__dirname, "../static/scss"),
     },
   },
+
+  devtool: isDev ? "source-map" : false,
+
   devServer: {
+    hot: true,
+
     proxy: {
       "/api": {
         target: process.env.BACKEND_URL,
         changeOrigin: true,
       },
     },
+  },
+
+  stats: {
+    children: false,
+    modules: false,
   },
 };
